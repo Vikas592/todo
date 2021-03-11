e33a994ff0fab46dfc7280d62651acb125e5dfbb
Vikas Choubey <vikas.choubey@successive.tech>
2021-03-11 16:35:30 +0530

    feat: react + redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Input from './Components/Input';
 import Item from './Components/Item'
 
 function App() {
-  const [todos, setTodos] = useState([{
-    id: Date(),
-    item: "task1"
-  }, {
-    id: Date() + 1,
-    item: "task2"
-  }])
+
+  const todos = useSelector(state => state)
+
 
   return (
     <div className="App">
-      <Input setTodos={setTodos} />
+      <Input />
+      <h5>My Todos</h5>
       <ul className="item-container">
-        {todos.map((todo) =>
-          <li key={todo.id} ><Item todo={todo} setTodos={setTodos} /></li>
+        {todos.map(todo =>
+          <li key={todo.id} ><Item todo={todo} /></li>
         )}
       </ul>
     </div>
--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react'
 import "./Item.css"
+import { useDispatch } from 'react-redux'
+import { editItem, updateStatus } from '../Redux/actions';
 
+function Item({ todo }) {
 
-function Item({ todo, setTodos }) {
-    const [done, setDone] = useState(false)
+    const dispatch = useDispatch();
     const [edit, setEdit] = useState(false)
 
     const handleDelete = () => {
-        if (done) {
-            setDone(false)
+        if (todo.status !== "pending") {
+            dispatch(
+                updateStatus(todo.id, todo.item, "pending")
+            )
             setEdit(false)
         }
-        else setDone(true)
+        else dispatch(
+            updateStatus(todo.id, todo.item, "completed")
+        )
     }
     const handleEdit = () => {
         setEdit(!edit)
@@ -19,14 +25,17 @@ function Item({ todo, setTodos }) {
 
     const handleChange = (event) => {
         const newTodo = { id: todo.id, item: event.target.value };
-        setTodos((prevTodos) => [...prevTodos.filter(todoItem => todoItem.id !== todo.id), newTodo]);
+        dispatch(editItem(
+            todo.id, event.target.value
+        ))
+        // setTodos((prevTodos) => [...prevTodos.filter(todoItem => todoItem.id !== todo.id), newTodo]);
         todo = newTodo
     }
     return (
         <div className="todoItem">
             <div className="todo-text" >
                 <i className="fas fa-notes-medical" style={{ color: "#4285F4" }}></i>
-                {done ?
+                {todo.status !== "pending" ?
                     <p style={{ textDecoration: "line-through" }}>{todo.item}</p>
                     :
                     edit
@@ -40,8 +49,8 @@ function Item({ todo, setTodos }) {
                 }
             </div>
 
-            <button className="buttons" disabled={done} onClick={handleEdit}>{edit ? <i className="fas fa-check"></i> : <i className="fas fa-pencil-alt"></i>}</button>
-            <button className="buttons" onClick={handleDelete} >{done ? <i className="fas fa-undo"></i> : <i className="fas fa-trash-alt"></i>}</button>
+            <button className="buttons" disabled={todo.status !== "pending"} onClick={handleEdit}>{edit ? <i className="fas fa-check"></i> : <i className="fas fa-pencil-alt"></i>}</button>
+            <button className="buttons" onClick={handleDelete} >{todo.status !== "pending" ? <i className="fas fa-undo"></i> : <i className="fas fa-trash-alt"></i>}</button>
         </div>
     )
 }
