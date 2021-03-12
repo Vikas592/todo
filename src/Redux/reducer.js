4b536c989a18084fc471286a9db5aeda60cd997e
Vikas Choubey <vikas.choubey@successive.tech>
2021-03-12 12:14:35 +0530

    refactor: edit feature

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -12,7 +12,12 @@ export const reducer = (state = initialState, action = {}) => {
         case ADD_TODO:
             return [...state, action.payload];
         case EDIT_TODO:
-            return [...state.filter(todo => todo.id !== action.payload.id), { id: action.payload.id, item: action.payload.item, status: "pending" }];
+            return [...state.map(todo => {
+                if (todo.id === action.payload.id)
+                    todo.item = action.payload.item
+                return todo;
+            })
+            ];
         case UPDATE_STATUS:
             return [...state.filter(todo => todo.id !== action.payload.id), { id: action.payload.id, item: action.payload.item, status: action.payload.status }]
         default:
