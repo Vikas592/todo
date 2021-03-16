4bec49c8b17ffd55884f56ed12dcc193ff3c8345
Vikas Choubey <vikas.choubey@successive.tech>
2021-03-16 12:37:05 +0530

    refactor: improve readability

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -6,6 +6,7 @@ import { editItem, updateStatus, editting } from '../Redux-Toolkit/redux-toolkit
 function Item({ todo }) {
     const [item, setItem] = useState(todo.item);
     const dispatch = useDispatch();
+
     const handleDelete = () => {
         dispatch(updateStatus({ id: todo.id }))
     }
@@ -27,20 +28,50 @@ function Item({ todo }) {
         <div className="todoItem">
             <div className="todo-text" >
                 <i className="fas fa-notes-medical" style={{ color: "#4285F4" }}></i>
-                {todo.isComplete &&
-                    <p style={{ textDecoration: "line-through" }}>{todo.item}</p>}
-                {!todo.isComplete && todo.editting &&
+                {todo.isComplete
+                    &&
+                    <p style={{ textDecoration: "line-through" }}>{todo.item}</p>
+                }
+                {!todo.isComplete
+                    &&
+                    todo.editting
+                    &&
                     <input value={item}
                         onChange={handleChange}
                         autoFocus={true}
                     />}
 
-                {!todo.isComplete && !todo.editting && <p  >{todo.item}</p>}
+                {!todo.isComplete
+                    &&
+                    !todo.editting
+                    &&
+                    <p  >{todo.item}</p>
+                }
 
             </div>
 
-            <button className="buttons" disabled={todo.isComplete} style={{ backgroundColor: todo.isComplete ? "grey" : "#4285f4" }} onClick={handleEdit}>{todo.editting ? <i className="fas fa-check"></i> : <i className="fas fa-pencil-alt"></i>}</button>
-            <button className="buttons" onClick={handleDelete} >{todo.isComplete ? <i className="fas fa-undo"></i> : <i className="fas fa-trash-alt"></i>}</button>
+            <button
+                className="buttons"
+                disabled={todo.isComplete}
+                style={{ backgroundColor: todo.isComplete ? "grey" : "#4285f4" }}
+                onClick={handleEdit}>
+                {todo.editting
+                    ?
+                    <i className="fas fa-check"></i>
+                    :
+                    <i className="fas fa-pencil-alt"></i>
+                }
+            </button>
+            <button
+                className="buttons"
+                onClick={handleDelete} >
+                {todo.isComplete
+                    ?
+                    <i className="fas fa-undo"></i>
+                    :
+                    <i className="fas fa-trash-alt"></i>
+                }
+            </button>
         </div>
     )
 }
